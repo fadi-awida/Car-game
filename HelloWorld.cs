950de3e0569f34adc0fb3ada56445126db0eadf4
fadi-awida <awida.fadi2010@gmail.com>
2024-12-07 10:24:37 +0200

    this is our prpject

---
+++ HelloWorld.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(2024);
+        print("Hello world");
+        print("Hi");
+          
+
+     
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
